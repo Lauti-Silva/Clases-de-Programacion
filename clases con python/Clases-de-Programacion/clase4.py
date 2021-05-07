93c36299d9850cce580064623fee19f3c417e08b
Lautaro silva <80924066+Lauti-Silva@users.noreply.github.com>
2021-05-07 19:55:43 -0300

    Add files via upload

---
+++ clases con python/Clases-de-Programacion/clase4.py
@@ -0,0 +1,33 @@
+#def suma(num1,num2,total):
+  #  total = num1 - num2 
+ #   print('dentro de la fundio', total)
+
+#numero1 = 3 
+#numero2 = 5
+#total = 0 
+#suma(numero1, numero2,total)
+
+#print('elvalor de la suma es',total)
+
+
+#from funciones import positivo_negativo
+
+#numero=int(input("ingrese un numero"))
+
+#if(positivo_negativo(numero)):
+ #   print("positivo")
+#else:
+ #   print("es negativo") 
+
+#from funciones import triangulo
+
+#base=int(input("ingrese base"))
+#altura=int(input("ingrese altura"))
+
+
+#print (area_rectangulo(base, altura 
+
+
+lista = [['lunes',3], ['Martes',4], ['Miercoles',2], ['Jueves',1],['Viernes',5]] 
+
+print(lista[2])
---
+++ clases con python/Clases-de-Programacion/funciones.py
@@ -0,0 +1,14 @@
+def positivo_negativo(numero):
+    """determinar si el numero es positivio(devulve true) o negativo(decuelve falso)"""
+    if (numero >= 0):
+        return True
+    else:
+        return False     
+
+
+def triangulo(base, altura):
+    """dtermminar el area (calcula el base*altura de un triangulo)"""
+    return base*altura 
+
+def 
+    
